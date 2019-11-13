fb0c09b82d5db8155507d490e71ea8b397739b2c
Andrea S Barr <andreabarr87@gmail.com>
2019-11-13 22:28:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
   array = sentence.split
   array_reverse = array.collect {|string| string.reverse}
-  sentence_reverse = array_reverse.join( )
+  sentence_reverse = array_reverse.join(" ")
 end
